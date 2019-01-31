3b747dd37ccf0a13ce39ad17470767eeea2f2c7f
Rick Colgan <39073136+RickColgan@users.noreply.github.com>
2019-01-30 19:48:15 -0600

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,13 @@
+# hello.py
+
+def dooutput():
+    print('Hello world!')
+    print('This is the manual version of code uploaded to Jenkins and Github for')
+    print('assignment 3.')
+
+def main():
+    dooutput()
+
+if __name__ == '__main__':
+    main()
+
